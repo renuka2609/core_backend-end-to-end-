e101ca446e0209634913c633f6e7feebc0721858
renuka2609 <deshpanderenuka153@gmail.com>
2026-02-09 15:58:54 +0530

    Delete vendors directory

--- vendors/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from orgs.models import Organization
-
-class Vendor(models.Model):
-    org = models.ForeignKey(Organization, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
