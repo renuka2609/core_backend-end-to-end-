c833419fa7bea12302dc1b7d418e0efb98880943
renuka2609 <deshpanderenuka153@gmail.com>
2026-02-09 15:57:41 +0530

    Delete responses directory

--- responses/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import ResponseViewSet
-
-router = DefaultRouter()
-router.register(r"responses", ResponseViewSet)
-
-urlpatterns = router.urls
