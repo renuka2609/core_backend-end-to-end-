52b2587b483b375c656431be779b86da8769adf8
renuka2609 <deshpanderenuka153@gmail.com>
2026-02-10 14:10:50 +0530

    Delete assessments directory

--- assessments/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import AssessmentViewSet
-
-router = DefaultRouter()
-router.register(r"assessments", AssessmentViewSet, basename="assessments")
-
-urlpatterns = router.urls
